a372be8fd7fda22c243f246bdbe7fcccf8912b28
annarekonvald <anna.rekonvald@gmail.com>
2021-04-09 12:13:13 +0300

    Conflicting import, imported name 'Context' is ambiguous

--- android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/ConnectycubeFlutterCallKitPlugin.kt
+++ android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/ConnectycubeFlutterCallKitPlugin.kt
@@ -24,7 +24,6 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 import android.content.SharedPreferences
-import android.content.Context
 
 
 /** ConnectycubeFlutterCallKitPlugin */
